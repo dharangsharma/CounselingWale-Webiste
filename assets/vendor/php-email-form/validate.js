6874d80cf70442449fa054f84b9fad0c5b415461
counselingwale <138315757+counselingwale@users.noreply.github.com>
2023-07-10 11:02:33 +0530

    revert

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -67,9 +67,9 @@
       if (data.trim() == 'OK') {
         thisForm.querySelector('.sent-message').classList.add('d-block');
         thisForm.reset(); 
-      } //else {
-        //throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action); 
-      //}
+      } else {
+        throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action); 
+      }
     })
     .catch((error) => {
       displayError(thisForm, error);
